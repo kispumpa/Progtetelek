d827b0bf20e8082a89bef0d463b0d2de3a0b9687
matulamarton <matulamarton@stud.uni-obuda.hu>
2025-02-25 18:16:27 +0100

    LinearisKereses function created for find the first prime number in vector

--- Progtetelek/Progtetelek.cpp
+++ Progtetelek/Progtetelek.cpp
@@ -2,48 +2,48 @@
 #include <vector>
 #include <iostream>
 
-std::vector<int> RandomFeltolto()
+std::vector<int> RandomFeltolto(int n)
 {
 	std::vector<int> tomb; //10 elemu tomb
 	std::random_device r; //random device
 	std::mt19937 generator(r()); //random generator
 	std::uniform_int_distribution<int> dist(1, 100); //random "intervallum"
 
-	for (size_t i = 0; i < 10; i++)
+	for (size_t i = 0; i < n; i++)
 	{
 		tomb.push_back(dist(generator));
 	}
 
 	return tomb;
 }
-std::vector<int> Manualis()
+std::vector<int> Manualis(int n)
 {
 	std::cout << "10 erteket kell megadnod" << std::endl;
 	std::vector<int> vektor;
 
-	for (size_t i = 0; i < 10; i++)
+	for (size_t i = 0; i < n; i++)
 	{
 		int bem;
 		std::cout << i + 1;
-		std::cout <<". ertek: ";
+		std::cout << ". ertek: ";
 		std::cin >> bem;
 		vektor.push_back(bem);
 	}
 	return vektor;
 }
-int Sorozatszamitas(std::vector<int> v)
+int Sorozatszamitas(std::vector<int> v, int n)
 {
 	int szum = 0;
-	for (size_t i = 0; i < 10; i++)
+	for (size_t i = 0; i < n; i++)
 	{
 		szum += v[i];
 	}
 	return szum;
 }
-int Megszamlalas(std::vector<int> v)
+int Megszamlalas(std::vector<int> v, int n)
 {
 	int db = 0;
-	for (size_t i = 0; i < 10; i++)
+	for (size_t i = 0; i < n; i++)
 	{
 		if (v[i] % 2 == 0) //paros-e az adott elem
 			db++;
@@ -51,10 +51,10 @@ int Megszamlalas(std::vector<int> v)
 
 	return db;
 }
-int Maximumkivalasztas(std::vector<int> v)
+int Maximumkivalasztas(std::vector<int> v, int n)
 {
 	int max = 0;
-	for (size_t i = 1; i < 10; i++)
+	for (size_t i = 1; i < n; i++)
 	{
 		if (v[i] > v[max])
 			max = i;
@@ -62,10 +62,34 @@ int Maximumkivalasztas(std::vector<int> v)
 
 	return max;
 }
+bool PrimszamE(int x)
+{
+	int i = 2;
+
+	while (i < x && x % i != 0)
+		i++;
+	bool prim = i == x;
+
+	return prim;
+}
+int LinearisKereses(std::vector<int> v, int n)
+{
+	int i = 0;
+	while (i < n && !PrimszamE(v[i]))
+		i++;
+
+	bool vanPrim = i <= n;
+
+	if (vanPrim)
+		return i;
+	else
+		return -1; //ilyen idx nem lehet
+}
 
 int main()
 {
-	std::vector<int> vek(10);
+	const int n = 10;
+	std::vector<int> vek(n);
 
 	std::cout << "Random szamu vektort szeretnel (r), vagy te adod meg az ertekeket (m)? \nIrd be a betut: ";
 	char bemenet;
@@ -74,18 +98,19 @@ int main()
 
 	if (bemenet == 'r')
 	{
-		vek = RandomFeltolto();
+		vek = RandomFeltolto(n);
 		veletlen = true;
 	}
-
 	else if (bemenet != 'm')
 		std::cout << "Rossz erteket adtal meg" << std::endl;
 	else
-		vek = Manualis();
+		vek = Manualis(n);
+
+	vek.shrink_to_fit();
 
 	if (veletlen)
 	{
-		for (size_t i = 0; i < 10; i++)
+		for (size_t i = 0; i < n; i++)
 		{
 			std::cout << i + 1;
 			std::cout << ". ertek: ";
@@ -94,14 +119,23 @@ int main()
 	}
 
 	std::cout << "\nElemek osszege: ";
-	std::cout << Sorozatszamitas(vek) << std::endl;
+	std::cout << Sorozatszamitas(vek, n) << std::endl;
 
 	std::cout << "\nParos szamok darabszama: ";
-	std::cout << Megszamlalas(vek) << std::endl;
+	std::cout << Megszamlalas(vek, n) << std::endl;
 
-	int idx = Maximumkivalasztas(vek);
+	int idx = Maximumkivalasztas(vek, n);
 	std::cout << "\nLegnagyobb elem indexe: ";
 	std::cout << idx;
-	std::cout << ". és erteke: ";
+	std::cout << ". es erteke: ";
 	std::cout << vek[idx] << std::endl;
+
+	int primIdx = LinearisKereses(vek, n);
+	if (primIdx < 0)
+		std::cout << "\nNincs primszam a vektorban." << std::endl;
+	else
+	{
+		std::cout << "\nElso primszam a vektorban: ";
+		std::cout << vek[primIdx];
+	}
 }
