0078f00c2037b93bbe6a1455639b5e4e944f194e
matulamarton <matulamarton@stud.uni-obuda.hu>
2025-02-25 16:47:12 +0100

    correct input test from console

--- Progtetelek/Progtetelek.cpp
+++ Progtetelek/Progtetelek.cpp
@@ -1,16 +1,39 @@
+#include "Generator.h"
 #include <vector>
 #include <iostream>
 
-std::vector<int> RandomFeltolto() 
+std::vector<int> RandomFeltolto()
 {
-    std::vector<int> tomb(10); //10 elemu tomb
+	std::vector<int> tomb(10); //10 elemu tomb
+	std::random_device r; //random device
+	std::mt19937 generator(r()); //random generator
+	std::uniform_int_distribution<int> dist(1, 100); //random "intervallum"
+
 	for (size_t i = 0; i < 10; i++)
 	{
-		//feltoltes random szamokkal
+		tomb.push_back(generateRandom(generator, dist));
 	}
+
+	return tomb;
 }
+//std::vector<int> Manualis()
+//{
+//
+//}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::vector<int> vek(10);
+
+	std::cout << "Random szamu vektort szeretnel (R), vagy te adod meg az ertekeket (M)? \nIrd be a betut: ";
+	char bemenet;
+	std::cin >> bemenet;
+
+	if (bemenet == 'R')
+		std::cout << "R-t adott meg";
+	//vek = RandomFeltolto();
+	else if (bemenet != 'M')
+		std::cout << "Rossz erteket adtal meg" << std::endl;
+	else
+		std::cout << "M-et adott meg";
 }
